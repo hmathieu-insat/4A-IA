04befed828791bb60712ff3595db9a16ae73f0dc
Sixt Romain <sixt@insa-10222>
2022-03-01 16:08:52 +0100

    Add heuristique1

--- TP1/taquin.pl
+++ TP1/taquin.pl
@@ -75,7 +75,7 @@ final_state([[a, b,  c],
 
 			 
    %********************
-   % AFFICHAGE D'UN ETAT
+   % AFFICHAGE DUN ETAT
    %********************
    % format :  write_state(?State) ou State est une liste de lignes a afficher
 
@@ -133,7 +133,7 @@ vertical_permutation(X,Y,S1,S2) :-
    append(Above, [Line3,Line4|Below], S2). % recompose S2 
 
    %***********************************************************************
-   % Retrait d'une occurrence X en position N dans une liste L (resultat R) 
+   % Retrait d une occurrence X en position N dans une liste L (resultat R) 
    %***********************************************************************
    % use case 1 :   delete(?N,?X,+L,?R)
    % use case 2 :   delete(?N,?X,?L,+R)   
@@ -150,15 +150,26 @@ delete(N,X,[Y|L], [Y|R]) :-
    %*******************
    
    %*******************************************************************
-   % Coordonnees X(colonne),Y(Ligne) d'une piece P dans une situation U
+   % Coordonnees X(colonne),Y(Ligne) d une piece P dans une situation U
    %*******************************************************************
 	% format : coordonnees(?Coord, +Matrice, ?Element)
 	% Définit la relation entre des coordonnees [Ligne, Colonne] et un element de la matrice
+	/*
+	Exemples
+	
+	?- coordonnees(Coord, [[a,b,c],[d,e,f]],  e).        % quelles sont les coordonnees de e ?
+	Coord = [2,2]
+	yes
+	
+	?- coordonnees([2,3], [[a,b,c],[d,e,f]],  P).        % qui a les coordonnees [2,3] ?
+	P=f
+	yes
+	*/
 
-	coordonnees([L,C], Mat, Elt) :- nth1(L,Mat,Ligne), nth1(C,Ligne, Elt).
-
-   % Definir ensuite le predicat malplace(P,U,F) qui est vrai si les coordonnes de P dans U et dans F sont differentes.
-   malplace(Elt, S1, S2) :- not((nth1(L,S1,Ligne), nth1(C,Ligne, Elt), nth1(L,S2,LigneF), nth1(C,LigneF,Elt))).
+	
+	coordonnees([L,C], Mat, Elt) :- true.  %********
+                                          % A FAIRE
+                                          %********
 
 											 
    %*************
@@ -166,39 +177,59 @@ delete(N,X,[Y|L], [Y|R]) :-
    %*************
    
 heuristique(U,H) :-
-    heuristique1(U, H).  % au debut on utilise l'heuristique 1 
-%   heuristique2(U, H).  % ensuite utilisez plutot l'heuristique 2  
+    heuristique1(U, H).  % au debut on utilise l heuristique 1 
+%   heuristique2(U, H).  % ensuite utilisez plutot l heuristique 2  
    
    
    %****************
    %HEURISTIQUE no 1
    %****************
-   % Nombre de pieces mal placees dans l'etat courant U
-   % par rapport a l'etat final F
+   % Nombre de pieces mal placees dans l etat courant U
+   % par rapport a l etat final F
    
    
-   % Suggestions : définir d'abord le prédicat coordonnees(Piece,Etat,Lig,Col) qui associe à une pièce présente dans Etat
+   % Suggestions : définir d abord le prédicat coordonnees(Piece,Etat,Lig,Col) qui associe à une pièce présente dans Etat
    % ses coordonnees (Lig= numero de ligne, Col= numero de Colonne)
    
    % Definir ensuite le predicat malplace(P,U,F) qui est vrai si les coordonnes de P dans U et dans F sont differentes.
-   % On peut également comparer les pieces qui se trouvent aux mêmes coordonnees dans U et dans H et voir s'il sagit de la
-   % même piece.
+   % On peut également comparer les pieces qui se trouvent aux memes coordonnees dans U et dans H et voir s il sagit de la
+   % meme piece.
    
-    % Definir enfin l'heuristique qui détermine toutes les pièces mal placées (voir prédicat findall) 
+    % Definir enfin l heuristique qui détermine toutes les pièces mal placées (voir prédicat findall) 
 	% et les compte (voir prédicat length)
+   
+   heuristique1(U, H) :-
+    	final_state(F),
+    	heuristique1(U,F,H).
 
-   
-    heuristique1(U, H) :- true.     %********
-                                    % A FAIRE
-                                    %********
-   
-   
+   heuristique1([],[],0).
+
+   heuristique1([L1|R1],[L2|R2],H) :-
+   	h1_line(L1,L2,Hline),
+    heuristique1(R1,R2,Hrest),
+    H is Hrest + Hline.
+
+	h1_line([],[],0).
+	h1_line([Elem1|R1],[Elem2|R2],H) :-
+    	h1_elem(Elem1,Elem2,Helem),
+    	h1_line(R1,R2,Hrest),
+    	H is Helem + Hrest.
+
+	h1_elem(Elem1,Elem1,0).
+	h1_elem(vide,_,0).
+	h1_elem(Elem1,Elem2,1) :-
+    	Elem1 \= Elem2,
+    	Elem1 \= vide.
+    
+    
+
+    
    %****************
    %HEURISTIQUE no 2
    %****************
    
    % Somme des distances de Manhattan à parcourir par chaque piece
-   % entre sa position courante et sa positon dans l'etat final
+   % entre sa position courante et sa positon dans l etat final
 
    
     heuristique2(U, H) :- true.     %********
